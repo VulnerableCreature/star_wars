414de7e0e99272f39468836bc4e56644aef11abf
Lev <otselsiy@mail.ru>
2023-06-13 09:25:05 +0300

    update

---
+++ resources/views/user/includes/menu.blade.php
@@ -0,0 +1,8 @@
+<div class="row align-items-center text-center alert alert-light border border-dark mb-3 mt-3">
+    <div class="col mx-2">
+        <a href="{{ route('main.index') }}" class="btn btn-outline-dark">Главная</a>
+    </div>
+    <div class="col mx-2">
+        <a href="" class="btn btn-outline-dark">Посты</a>
+    </div>
+</div>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,2 +1,21 @@
 @extends('layouts.app')
 @section('title', 'Профиль')
+
+@section('content')
+    @include('user.includes.menu')
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ Auth::user()->name }}</div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <div>It's the post</div>
+                    <div class="justify-content-end">
+                        <a href="" class="btn btn-primary">Просмотреть</a>
+                        <a href="" class="btn btn-primary">Редактировать</a>
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
